ddfbdd8bd47b2b59d08b0d36136346b4f3a47d10
OmariLawrence <originalolawrence@gmail.com>
2019-01-27 11:46:44 -0400

    first commit

---
+++ Assets/playermovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class playermovement : MonoBehaviour
+{
+    public float speed;
+    //    public Rigidbody rb;
+    public Animator playAnim;
+
+    private void Start()
+    {
+        playAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            playAnim.SetBool("walking", true);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+            playAnim.SetBool("walking", true);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            playAnim.SetBool("walking", true);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+            playAnim.SetBool("walking", true);
+        }
+        if(Input.GetAxis("Horizontal")==0 && Input.GetAxis("Vertical") == 0)
+        {
+            playAnim.SetBool("walking", false);
+        }
+//        if (Input.GetKeyDown("space"))
+//        {
+//            rb.AddForce(Vector3.up * 250);
+//        }
+
+    }
+}
