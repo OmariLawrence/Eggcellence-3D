ea32e9748f8a99e1b0ecfe83d889daf444c86b17
OmariLawrence <originalolawrence@gmail.com>
2019-03-18 18:06:31 -0400

    menu added

---
+++ Assets/scripts/mainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/scripts/shutdown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class shutdown : MonoBehaviour
+{
+    public NetworkLobbyManager nlm = null;
+    public NetworkManagerHUD hud = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(nlm!=null && hud != null)
+        {
+            nlm.enabled = true;
+            hud.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (nlm != null && hud != null)
+            {
+                //nlm.enabled = false;
+                hud.enabled = false;
+            }
+        }
+        if(SceneManager.GetActiveScene().name == "Lobby")
+        {
+            //nlm.enabled = true;
+            hud.enabled = true;
+        }
+        else
+        {
+            //nlm.enabled = false;
+            hud.enabled = false;
+        }
+        
+    }
+}
