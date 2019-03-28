07f4bee08920c7b8e76c7f7de135a1fd4fcf1cc6
OmariLawrence <originalolawrence@gmail.com>
2019-03-28 19:06:06 -0400

    nametags v1

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+
+    [SyncVar]
+    public string username = "Username";
+
+    [Command]
+    void CmdSetUsername(string user)
+    {
+        username = user;
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        string myUsername = string.Format("Player {0}", netId.Value);
+        CmdSetUsername(myUsername);
+    }
+}
---
+++ Assets/scripts/PlayerNamePlate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNamePlate : MonoBehaviour
+{
+    [SerializeField]
+    private Text usernameText;
+
+    [SerializeField]
+    private Player player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        usernameText.text = player.username;
+    }
+}
